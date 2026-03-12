bde569bf0d1d8f1d452bd31e5600a86f7ffb289c
Garvit Chauhan <madhur.chauhan2005@gmail.com>
2026-03-12 18:49:25 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://garvit20005.github.io",
+  base: "/Portfolio/",
   vite: {
     plugins: [tailwindcss()],
   },
